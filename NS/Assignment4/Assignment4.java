26fa9f1bec47551715d850af04bcf27a83df3d4c
LasaniHussain <lasanihussain@gmail.com>
2021-11-14 16:16:07 +0530

    Key generation

--- NS/Assignment4/Assignment4.java
+++ NS/Assignment4/Assignment4.java
@@ -1,23 +1,46 @@
 import java.util.*;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+ 
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.swing.plaf.synth.SynthStyle;
 class AuthenticationServer{
-    private Hashtable<String,String> symm_keys = new Hashtable<String,String>();
+    private Hashtable<String,SecretKey> symm_keys = new Hashtable<String,SecretKey>();
     String id;
-    void addSymmKey(String id,String symm_key){
-
+    AuthenticationServer()
+    {
+        Random rn = new Random();
+        id= String.valueOf(300+rn.nextInt(100));
+    }
+    void addSymmKey(String id,SecretKey symm_key){
+        symm_keys.put(id,symm_key);
+    }
+    void DiplaySymmKeys()
+    {   System.out.println(symm_keys);
+        /*for(int i=0;i<symm_keys.size();i++)
+        System.out.println(symm_keys.);*/
     }
     String service_request(String id, int serviceType){
-        return "";hello
+        return "";
     }
 }
 class Client{
     String as_symm_key;
     String client_id;
     String timestamped_doc;//this can either by the doc timestamped by ts or recieved form another client
+    
+    Client()
+    {   Random rn = new Random();
+        client_id= String.valueOf(rn.nextInt(100));
+    }
     void start(){
 
     }
     void send_request_to_as(int serviceType){
-        add
+        
     }
     void request_ts(String ticket_from_as){
 
@@ -40,9 +63,13 @@ class TimestampingServer{
     String as_symm_key;
     String rsa_public_key;//maybe not needed here
     private String rsa_private_key;
-    void addPrivateKey(String private_key){
-
+    void addPrivateKey(String private_key){}
+    TimestampingServer()
+    {
+        Random rn = new Random();
+        id= String.valueOf(100+rn.nextInt(100));
     }
+
     String timestamping_request(String id, String ticket,String request){
         return "";
     }
@@ -51,6 +78,11 @@ class PublicKeyServer{//changing name for CA
     String id;
     String as_symm_key;
     private Hashtable<String,String> public_keys = new Hashtable<String,String>();
+    PublicKeyServer()
+    {
+        Random rn = new Random();
+        id= String.valueOf(200+rn.nextInt(100));
+    }
     void addPublicKey(String id,String public_key){
 
     }
@@ -59,12 +91,50 @@ class PublicKeyServer{//changing name for CA
     }
 }
 public class Assignment4{
-    static void init(){
-
+    public static SecretKey genDesKey()
+    {   SecretKey key=null;
+        try {
+ 
+            // generate secret key using DES algorithm
+            key = KeyGenerator.getInstance("DES").generateKey();
+            //return key;
+        }
+        catch(Exception e)
+        {
+        }
+        return key;
+    }
+    static void init(Client cl1,Client cl2,TimestampingServer ts,PublicKeyServer pks,AuthenticationServer as){
+        //symm key between client1 and AS
+        SecretKey cl1_AS=genDesKey();
+        as.addSymmKey(cl1.client_id, cl1_AS);;
+        
+        //symm key between client2 and AS
+        SecretKey cl2_AS=genDesKey();
+        as.addSymmKey(cl2.client_id, cl2_AS);;
+        
+        //symm key between PKS and AS
+        SecretKey PKS_AS=genDesKey();
+        as.addSymmKey(pks.id, PKS_AS);
+        
+        //symm key between TS and AS
+        SecretKey TS_AS=genDesKey();
+        as.addSymmKey(ts.id, TS_AS);
+        
+        as.DiplaySymmKeys();
+        //
     }
     public static void main(String[] args){
-        init();
-        File file = new File("/home/lasani/IIIT Delhi/Network Security/Assignment 4/Assignment4 NS.pdf");
+        //init();
+        Client cl1=new Client();
+        Client cl2=new Client();
+        TimestampingServer ts=new TimestampingServer();
+        PublicKeyServer pks=new PublicKeyServer();
+        AuthenticationServer as=new AuthenticationServer();
+        System.out.println(cl1.client_id+" "+cl2.client_id+" "+ts.id+" "+pks.id+" "+as.id);
+        init(cl1,cl2,ts,pks,as);
+        //System.out.println();
+        /*File file = new File("/home/lasani/IIIT Delhi/Network Security/Assignment 4/Assignment4 NS.pdf");
       PDDocument document = PDDocument.load(file);
       //Instantiate PDFTextStripper class
       PDFTextStripper pdfStripper = new PDFTextStripper();
@@ -86,6 +156,6 @@ public class Assignment4{
     String hashtext = bigInt.toString(16);
     System.out.println(hashtext);
       //Closing the document
-      document.close();
+      document.close();*/
     }
 }
